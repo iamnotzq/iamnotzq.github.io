df4f57079c897ecece886d005b2b8b94e51fbf27
iamnotzq <evilbefall98@gmail.com>
2024-04-03 21:44:45 +0800

    - update details

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 import './footer.css';
 
 const Footer = () => {
+  // Get the current year
+  const year = new Date().getFullYear();
+
   return (
     <footer className='footer'>
-        Copyright bla bla
+        {/* Render the dynamic year in the copyright message */}
+        Copyright &copy; {year} Lam Zhi Qiang. All Rights Reserved
     </footer>
-  )
+  );
 }
 
 export default Footer;
